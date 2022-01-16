70acb539a20904a575ddd4f10ec8d5036e0b76c2
headtop <headtop@mail.ru>
2022-01-16 08:56:12 +0300

    debug: format text

--- android/app/src/main/java/com/my_gps_tracker/MyLocation.kt
+++ android/app/src/main/java/com/my_gps_tracker/MyLocation.kt
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.IBinder
 import android.os.Looper
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
@@ -31,18 +30,15 @@ class MyLocation : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         initializeDbRef()
         val CHANNELID = "Foreground Service ID"
-        val channel = NotificationChannel(
-            CHANNELID,
-            CHANNELID,
-            NotificationManager.IMPORTANCE_LOW
-        )
+        val channel = NotificationChannel(CHANNELID, CHANNELID, NotificationManager.IMPORTANCE_LOW)
         getSystemService(NotificationManager::class.java).createNotificationChannel(channel)
-        val notification = Notification.Builder(this, CHANNELID)
-            .setContentText("Service location is running")
-            .setContentTitle("Service location enable")
-            .setSmallIcon(R.drawable.android)
+        val notification =
+                Notification.Builder(this, CHANNELID)
+                        .setContentText("Service location is running")
+                        .setContentTitle("Service location enable")
+                        .setSmallIcon(R.drawable.android)
 
-        getLastLocation("location")///заменить на нормальное
+        getLastLocation("location") // /заменить на нормальное
         startForeground(1002, notification.build())
 
         return super.onStartCommand(intent, flags, startId)
@@ -55,45 +51,48 @@ class MyLocation : Service() {
         super.onDestroy()
     }
 
-
     override fun onBind(intent: Intent): IBinder? {
         return null
     }
-
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun getLastLocation(devId: String) {
         initializeDbRef()
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(
+                                this,
+                                Manifest.permission.ACCESS_COARSE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED
+        ) {}
         val mLocationRequest = LocationRequest.create()
         mLocationRequest.interval = 6000
         mLocationRequest.fastestInterval = 5000
         mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
 
-        val mLocationCallback: LocationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult) {
-                for (location in locationResult.locations) {
-                    if (location != null) {
-                        //TODO: UI updates.
-//                        Log.d("location", "${location.latitude},${location.longitude}")
-                        database.child("location").child("latitude").setValue(location.latitude)
-                        database.child("location").child("longitude").setValue(location.longitude)
+        val mLocationCallback: LocationCallback =
+                object : LocationCallback() {
+                    override fun onLocationResult(locationResult: LocationResult) {
+                        for (location in locationResult.locations) {
+                            if (location != null) {
+                                // TODO: UI updates.
+                                //                        Log.d("location",
+                                // "${location.latitude},${location.longitude}")
+                                database.child("location")
+                                        .child("latitude")
+                                        .setValue(location.latitude)
+                                database.child("location")
+                                        .child("longitude")
+                                        .setValue(location.longitude)
+                            }
+                        }
                     }
                 }
-            }
-        }
-        fusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper()!!);
+        fusedLocationClient.requestLocationUpdates(
+                mLocationRequest,
+                mLocationCallback,
+                Looper.myLooper()!!
+        )
     }
-
-
 }
